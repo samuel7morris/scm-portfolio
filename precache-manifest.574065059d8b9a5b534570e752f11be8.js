8826de5f86d995a9a2c3d1e48b2023569a775e53
Sam Morris <63416066+samuel7morris@users.noreply.github.com>
2020-08-31 08:10:02 -0600

    Updates

---
+++ precache-manifest.574065059d8b9a5b534570e752f11be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323cb28b219ea7ecae5d706b612e38e4",
+    "url": "/scm-portfolio/index.html"
+  },
+  {
+    "revision": "ff16c6ea291ae68ae667",
+    "url": "/scm-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0c7d90fb2376604e38fd",
+    "url": "/scm-portfolio/static/css/main.d79190f9.chunk.css"
+  },
+  {
+    "revision": "ff16c6ea291ae68ae667",
+    "url": "/scm-portfolio/static/js/2.87ce80b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/scm-portfolio/static/js/2.87ce80b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7d90fb2376604e38fd",
+    "url": "/scm-portfolio/static/js/main.cd632ab8.chunk.js"
+  },
+  {
+    "revision": "625ac211b39c7677bb4b",
+    "url": "/scm-portfolio/static/js/runtime-main.6ff96799.js"
+  }
+]);
